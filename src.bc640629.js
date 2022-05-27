90c4ddf8f2243775c64348cdd36feb85182cad60
SergeyBelozub <99657458+SergeyBelozub@users.noreply.github.com>
2022-05-27 17:12:52 +0000

    Deploying to gh-pages from @ SergeyBelozub/Group-IceCream@55e47ef9872d702bc85213ea5db0b4cf8e1eed4f 🚀

--- src.bc640629.js
+++ src.bc640629.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/Group-IceCream/src.eef113bb.js.map
+//# sourceMappingURL=/Group-IceCream/src.bc640629.js.map
